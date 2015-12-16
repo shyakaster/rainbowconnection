07ab02b54da73a034944958a397c17ccc32543d7
kenedyivan <kenedyakenaivan@gmail.com>
2015-12-16 18:21:35 +0300

    style check

---
+++ app/helpers/rainbowconnection_helper.rb
@@ -0,0 +1,2 @@
+module RainbowconnectionHelper
+end
---
+++ test/controllers/rainbowconnection_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RainbowconnectionControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
